7c90a122aba9fde27dd429288671b05188529e96
niC3 <florian-albert@gmx.de>
2012-10-30 14:58:00 +0100

    Comments

--- StressYourself/src/de/dhbw/stress_yourself/Admin.java
+++ StressYourself/src/de/dhbw/stress_yourself/Admin.java
@@ -3,6 +3,9 @@ package de.dhbw.stress_yourself;
 
 import javax.swing.*;
 
+/**
+ * need this imports for createMD5()
+ */
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -16,27 +19,59 @@ import java.security.NoSuchAlgorithmException;
 public class Admin {
 	
 	public static JPanel aPanel = new JPanel();
-
+	
+	/**
+	 * creates a JPanel
+	 * the content is: Usermanagement / Testmanagement
+	 * @return
+	 */
 	public JPanel loadAdminGUI() {
 		aPanel.setBounds(10, 25, 500, 350);
 		return aPanel;
 	}
-
+	
+	/**
+	 * 
+	 * @param username - String with the username which should be created
+	 * @param password - String with the password of the user
+	 * @param type - depends on weather he is an admin(a) or an user(u) 
+	 * @return true if user was created / false is some param is not ok
+	 */
 	public boolean createUser(String username, String password, String type) {
 		//rufe saveUser() aus UserData auf
 		return false;
 	}
-
+	
+	
+	/**
+	 * 
+	 * @param username - String with the username of the user who should be deleted
+	 * @return true if the user is marked as deleted. Will be saved after regular closing 
+	 * 			of the programm
+	 */
 	public boolean deleteUser(String username) {
 		// rufe deleteUser() aus UserData auf
 		return false;
 	}
-
+	
+	
+	/**
+	 * 
+	 * @param username - String with the username from whom u wannt to change the password
+	 * @param newpassword - String with the new password of the named user
+	 * @return true if the password was changed !the password isn't written in xml yet!
+	 * 			if the programm chrashes the changes are lost!
+	 */
 	public boolean changePassword(String username, String newpassword) {
 		// changePassword(username, password) aus UserData
 		return false;
 	}
 	
+	/**
+	 * 
+	 * @param password - Passwordstring with the userpassword
+	 * @return the passwordstring transformed in MD5
+	 */
 	private String createMD5(String password){
 		String hashword = null;
 		try{
