0278feb52987f388d3769bae7b02fd6445b2f3d6
niC3 <florian-albert@gmx.de>
2012-10-30 09:15:22 +0100

    createMD5()

--- StressYourself/src/de/dhbw/stress_yourself/Admin.java
+++ StressYourself/src/de/dhbw/stress_yourself/Admin.java
@@ -1,5 +1,15 @@
 package de.dhbw.stress_yourself;
 
+import java.io.File;
+import javax.xml.parsers.*;
+import org.w3c.dom.Document;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+
 public class Admin {
 	
 	public boolean loadAdminGUI(){
@@ -19,7 +29,16 @@ public class Admin {
 	}
 	
 	private String createMD5(String password){
-		return "";
+		String hashword = null;
+		try{
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			md5.update(password.getBytes());
+			BigInteger hash = new BigInteger(1, md5.digest());
+			hashword = hash.toString(16);
+		} catch (NoSuchAlgorithmException nsae) {
+			
+		}
+		return hashword;
 	}
 
 }
