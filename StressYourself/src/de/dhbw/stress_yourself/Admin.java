8d8824fc3af8f3b261528815f7b09a2cc205aa3d
niC3 <florian-albert@gmx.de>
2012-11-06 14:19:01 +0100

    even more code

--- StressYourself/src/de/dhbw/stress_yourself/Admin.java
+++ StressYourself/src/de/dhbw/stress_yourself/Admin.java
@@ -1,19 +1,21 @@
 package de.dhbw.stress_yourself;
 
 import java.awt.Color;
-import java.text.Format;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
-import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextField;
-import javax.swing.table.TableColumn;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
 
 
 
@@ -26,7 +28,42 @@ import javax.swing.table.TableColumn;
  * @author Florian Albert <floria-albert@gmx.de>
  */
 
+
 public class Admin {
+	/**
+	 * class SetMaxText
+	 * 		- with this class you are able to
+	 * 		  set the maxlength of an JTextField / JPasswordField
+	 * 		  in a very easy way.
+	 *		- use: someTextField.setDocument(new SetMaxText(int Max));
+	 */
+	public class SetMaxText extends PlainDocument {
+	  private int limit;
+	  // optional uppercase conversion
+	  private boolean toUppercase = false;
+	  
+	  SetMaxText(int limit) {
+	   super();
+	   this.limit = limit;
+	   }
+	   
+	  SetMaxText(int limit, boolean upper) {
+	   super();
+	   this.limit = limit;
+	   toUppercase = upper;
+	   }
+	 
+	  public void insertString
+	    (int offset, String  str, AttributeSet attr)
+	      throws BadLocationException {
+	   if (str == null) return;
+
+	   if ((getLength() + str.length()) <= limit) {
+	     if (toUppercase) str = str.toUpperCase();
+	     super.insertString(offset, str, attr);
+	     }
+	   }
+	}
 
 	private JPanel pnlUserManagement = new JPanel();
 	private JPanel pnlTestManagement = new JPanel();
@@ -50,11 +87,13 @@ public class Admin {
 	
 	private JPasswordField pfPassword = new JPasswordField();
 	private JTextField tfUsername = new JTextField();
-	private JFormattedTextField tfTime = new JFormattedTextField();
+	private JTextField tfTime = new JTextField();
 	
 	
-	private JTable activeModules;
-	private JList<String> availableModules;
+	private DefaultListModel<String> dlActiveModules = new DefaultListModel<String>();
+	private DefaultListModel<String> dlAvailableModules = new DefaultListModel<String>();
+	private JList<String> lActiveModules;
+	private JList<String> lAvailableModules;
 	
 	private JScrollPane spActiveModules;
 	private JScrollPane spAvailableModules;
@@ -63,10 +102,51 @@ public class Admin {
 	private JLabel lblUserManagement = new JLabel("User Management:");
 	private JLabel lblUsername = new JLabel("User:");
 	private JLabel lblPassword = new JLabel("Password:");
+	private JLabel lblSetTime = new JLabel("Set Time:");
 	
 	
 	private UserData users;
 	private Parameter params;
+	
+	private ActionListener selectBtnFunction = new ActionListener() {
+		private int index;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getSource().equals(btnActivateModule)) {
+				System.out.println(lAvailableModules.getSelectedValue());
+				if(tfTime.getText().length() > 0) {
+					if(!lAvailableModules.isSelectionEmpty()) {
+						dlActiveModules.addElement(lAvailableModules.getSelectedValue()+" "+tfTime.getText());
+						
+						index = lAvailableModules.getSelectedIndex();
+						System.out.println(index);
+						lAvailableModules.remove(index);
+						dlAvailableModules.remove(index);
+						
+						lAvailableModules.revalidate();
+						spAvailableModules.revalidate();
+						
+						lActiveModules.revalidate();
+						spActiveModules.revalidate();
+					}
+				}
+			} else if(e.getSource().equals(btnInactivateModule)) {
+				
+			} else if(e.getSource().equals(btnChangePassword)) {
+				
+			} else if(e.getSource().equals(btnCreateUser)) {
+				
+			} else if(e.getSource().equals(btnDeleteUser)) {
+				
+			} else if(e.getSource().equals(btnModuleDown)) {
+				
+			} else if(e.getSource().equals(btnModuleUp)) {
+				
+			}
+		}
+		
+	};
 	
 	public static JPanel aPanel;	
 	
@@ -122,15 +202,19 @@ public class Admin {
 		pnlUserManagement.add(lblPassword);
 		
 		pfPassword.setBounds(110, 30, TEXTFIELDWIDTH, COMPONENTHEIGHT);
+		pfPassword.setDocument(new SetMaxText(8));
 		pnlUserManagement.add(pfPassword);
 		
 		btnCreateUser.setBounds(75, 60, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnCreateUser.addActionListener(selectBtnFunction);
 		pnlUserManagement.add(btnCreateUser);
 		
 		btnChangePassword.setBounds(75, 85, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnChangePassword.addActionListener(selectBtnFunction);
 		pnlUserManagement.add(btnChangePassword);
 		
 		btnDeleteUser.setBounds(75, 110, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnDeleteUser.addActionListener(selectBtnFunction);
 		pnlUserManagement.add(btnDeleteUser);
 	
 	}
@@ -141,54 +225,75 @@ public class Admin {
 		pnlTestManagement.setBackground(backgroundColor);
 		
 		btnActivateModule.setBounds(215, 40, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnActivateModule.addActionListener(selectBtnFunction);
 		pnlTestManagement.add(btnActivateModule);
 		
-		tfTime.setBounds(260, 100, 60, 20);
-		tfTime.setColumns(3);
+		lblSetTime.setBounds(215, 80, BUTTONWIDTH, COMPONENTHEIGHT);
+		pnlTestManagement.add(lblSetTime);
+		
+		tfTime.setBounds(260, 100, 45, 20);
+		tfTime.setDocument(new SetMaxText(3));
+		tfTime.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				if((e.getKeyCode() != 8 && e.getKeyChar() < (char)48) || e.getKeyChar() > (char) 57) {
+					tfTime.setText(tfTime.getText().substring(0, tfTime.getText().length()-1));
+				}
+			}
+		});
+		
 		
 		pnlTestManagement.add(tfTime);
 		
 		btnInactivateModule.setBounds(215, 160, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnInactivateModule.addActionListener(selectBtnFunction);
 		pnlTestManagement.add(btnInactivateModule);
 		
 		btnModuleUp.setBounds(215, 200, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnModuleUp.addActionListener(selectBtnFunction);
 		pnlTestManagement.add(btnModuleUp);
 		
 		btnModuleDown.setBounds(215, 240, BUTTONWIDTH, COMPONENTHEIGHT);
+		btnModuleDown.addActionListener(selectBtnFunction);
 		pnlTestManagement.add(btnModuleDown);
 		
-		
-		
-		
-		
-		activeModules = new JTable();
-		activeModules.setBounds(5, 20, 200, 300);
-		activeModules.addColumn(new TableColumn(1));
-		
-		
-		DefaultListModel<String> test = new DefaultListModel<String>();//
-		
-		
-		
-		String[] mydata = new String[100];
-		String[] mydata1 = new String[100];
-		for (int i=0;i<100;i++) {
-			mydata[i] = ""+i;
-			mydata1[i] = ""+(100-i);
+		try {
+			for (int i = 0; i < params.getAvailableModules().size();i++) {
+				dlAvailableModules.add(i, params.getAvailableModules().get(i).getName());
+				if(i == params.getAvailableModules().size()-1) {
+					dlAvailableModules.add(i+1, "22");
+				}
+			}
+			for (int i = 0; i < params.getConfiguration().size();i++) {
+				dlActiveModules.add(i, params.getConfiguration().get(i).getName()+" "+params.getConfiguration().get(i).getTime());
+				if(i == params.getConfiguration().size()-1) {
+					dlActiveModules.add(i+1, " 22");
+				}
+			}
+		}catch(IndexOutOfBoundsException e) {
+			System.out.println("One list is empty");
 		}
 		
-		for (int i = 0; i < params.getAvailableModules().size();i++) {
-			System.out.println(params.getAvailableModules().get(i).getName());
-			System.out.println(""+params.getAvailableModules().size());
-			test.add(i, params.getAvailableModules().get(i).getName());//
-		}
+		lActiveModules = new JList<String>(dlActiveModules);
+		spActiveModules = new JScrollPane(lActiveModules);
+		spActiveModules.setBounds(5, 20, 200, 300);
 		
-		pnlTestManagement.add(activeModules);
+		pnlTestManagement.add(spActiveModules);
 		
-		
-		availableModules = new JList<String>(test);
-		
-		spAvailableModules = new JScrollPane(availableModules);
+		lAvailableModules = new JList<String>(dlAvailableModules);
+		spAvailableModules = new JScrollPane(lAvailableModules);
 		spAvailableModules.setBounds(375, 20, 200, 300);
 		
 		pnlTestManagement.add(spAvailableModules);
