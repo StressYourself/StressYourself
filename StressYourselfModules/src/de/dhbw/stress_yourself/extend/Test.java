52d3f4b5d6aabd6e99444555da12ec4bd79c9dd9
Tobias Röding <tobias.roeding@googlemail.com>
2012-11-07 13:33:40 +0100

    fix

--- StressYourselfModules/src/de/dhbw/stress_yourself/extend/Test.java
+++ StressYourselfModules/src/de/dhbw/stress_yourself/extend/Test.java
@@ -1,28 +1,47 @@
 package de.dhbw.stress_yourself.extend;
 
+import java.util.LinkedList;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class Test {
 
-	private String[] arithmeticOperators = { "+", "-", "*", "/", "%" };
-	private String[] compareOperators = { "==", "!=", "<", ">", "<=", ">=" };
-	private String[] incdecOperators = { "++", "--" };
-	private String[] logicalOperators = { "!", "&&", "&", "||", "|", "^" };
-	
+	private static final String[] arithmeticOperators = { "+", "-", "*", "/", "%" };
+	private static final String[] compareOperators = { "==", "!=", "<", ">", "<=", ">=" };
+	private static final String[] incdecOperators = { "++", "--" };
+	private static final String[] logicalOperators = { "!", "&&", "&", "||", "|", "^" };
+	private static final String[] bool = {"==","!="};
 
+	
+	
+	
 	public static void main(String args[]) {
 		System.out.println("result " + evaluateTest(generateTest(1)));
 		System.out.println("result " + evaluateTest(generateTest(2)));
 		System.out.println("result " + evaluateTest(generateTest(3)));
 	}
+	
+	public static int randomNumber(int high, int low){
+		return (int) (Math.random() * (high - low) + low);
+	}
 
 	public static String generateTest(int difficulty) {
+		
+		String test = "";
+		test += String.valueOf(randomNumber(9, 0));
+		test += arithmeticOperators[randomNumber(arithmeticOperators.length -1, 0)];
+		test += String.valueOf(randomNumber(9, 0));
+		test += bool[randomNumber(bool.length -1, 0)];
+		test += String.valueOf(randomNumber(9, 0));
+		test += arithmeticOperators[randomNumber(arithmeticOperators.length -1, 0)];
+		test += String.valueOf(randomNumber(9, 0));
+		
 		String result = "";
 		switch (difficulty) {
 		case 1:
-			result = "1 && 1";
+			result = test;
 			break;
 		case 2:
 			result = "1+1 && 2-1";
