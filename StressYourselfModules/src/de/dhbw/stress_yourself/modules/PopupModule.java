18418a6bef799e48aabb35b816aac9a47ca0ab70
niC3 <florian-albert@gmx.de>
2012-11-12 07:53:37 +0100

    popupmodule

--- StressYourselfModules/src/de/dhbw/stress_yourself/modules/PopupModule.java
+++ StressYourselfModules/src/de/dhbw/stress_yourself/modules/PopupModule.java
@@ -1,5 +1,7 @@
 package de.dhbw.stress_yourself.modules;
 
+import java.awt.Color;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -7,126 +9,196 @@ import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.JTextPane;
-
 import de.dhbw.stress_yourself.extend.ModuleClass;
+
 
 public class PopupModule extends ModuleClass {
 
-	private final String moduleName = "PopupModule";
-	private final String moduleArea = "Reaction";
-	private final String moduleDescription = "Example Description";
+		private final String moduleName = "PopupModule";
+		private final String moduleArea = "Reaction";
+		private final String moduleDescription = "Example Description";
+		private int timePerButton;
+		private int numberOfButtons;
+		private int buttonCounter;
+		private int buttonClicked = 0;
+		
+		public final int FRAMEWIDTH = 900;
+		public final int FRAMEHEIGHT = 700;
+		private final int BUTTONHEIGHT = 20;
+		private final int BUTTONWIDTH = 20;
 
-	private ArrayList<Boolean> results = new ArrayList<Boolean>();
-	private int buttonCount = 1;
-	private int nextButtonIntervall = 3000;
-
-	public PopupModule(Object o, Integer difficulty, Integer time) {
-		super(o, difficulty.intValue(), time.intValue());
-		setTimerIntervall();
-	}
-
-	public void setTimerIntervall() {
-		switch (getDifficulty()) {
-		case (0):
-			break;
-		case (1):
-			nextButtonIntervall = 4000;
-			break;
-		case (2):
-			nextButtonIntervall = 3000;
-			break;
+		public PopupModule(Object o, Integer difficulty, Integer time) {
+			super(o, difficulty.intValue(), time.intValue());
+			initTestValues();
 		}
-	}
-
-	public JPanel getModuleJPanel() {
-		return new ModuleGUI();
-	}
-
-	@Override
-	public String getModuleName() {
-		return moduleName;
-	}
-
-	@Override
-	public String getModuleArea() {
-		return moduleArea;
-	}
-
-	@Override
-	public String getModuleDescription() {
-		return moduleDescription;
-	}
-
-	class ModuleGUI extends JPanel implements ActionListener {
-		private static final long serialVersionUID = 1L;
-		private ArrayList<JButton> buttons = null;
-		private JTextPane pane = new JTextPane();
-		private JTextField captchaText = new JTextField();
-		private JButton nextCaptchaButton = new JButton("next captcha");
-		private JButton nextModuleButton = new JButton("next module");
-		private JPanel thisPanel = this;
-
-		public ModuleGUI() {
-			buttons = new ArrayList<JButton>();
-			init();
-			setLayout(null);
-			setBounds(0, 0, 800, 600);
+		
+		/**
+		 * 
+		 */
+		public void initTestValues() {
+			switch (getDifficulty()) {
+			case 0:
+				timePerButton = 2000;
+				break;
+			case 1:
+				timePerButton = 1500;
+				break;
+			case 2:
+				timePerButton = 750;
+				break;
+			}
+			numberOfButtons = (getTime() / timePerButton);
+			buttonCounter = 0;
 		}
-
-		public void registerButton(JButton button) {
-			buttons.add(button);
-		}
-
-		public void init() {
-			pane.setText("CaptchaCharSequenceModule");
-			pane.setBounds(50, 50, 175, 30);
-			this.add(pane);
-
-			captchaText.setBounds(50, 210, 150, 30);
-			this.add(captchaText);
-
-			nextCaptchaButton.setBounds(220, 210, 70, 30);
-			registerButton(nextCaptchaButton);
-			nextCaptchaButton.addActionListener(this);
-			this.add(nextCaptchaButton);
-
-			nextModuleButton.setBounds(230, 50, 120, 30);
-			registerButton(nextModuleButton);
-			nextModuleButton.addActionListener(this);
-			this.add(nextModuleButton);
-			setNextModuleTimer(getTime(), new NextModule());
+		
+		public JPanel getModuleJPanel() {
+			return new ModuleGUI();
 		}
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			switch (buttons.indexOf(e.getSource())) {
-			case 0:// nextCaptchaButton
+		public String getModuleName() {
+			return moduleName;
+		}
+
+		@Override
+		public String getModuleArea() {
+			return moduleArea;
+		}
+
+		@Override
+		public String getModuleDescription() {
+			return moduleDescription;
+		}
+		
+		public JButton generateTest() {
+			
+			JButton testButton = new JButton("X");
+			
+			testButton.setBackground(Color.ORANGE);
+			testButton.setSize(BUTTONWIDTH, BUTTONHEIGHT);
+			testButton.setLocation(generateButtonLocation());
+			buttonCounter++;
+			
+			return testButton;
+		}
+		
+		private Point generateButtonLocation() {
+			int x = -1;
+			int y = -1;
+			do {
+				x = myRandom( 20, FRAMEWIDTH-20);
+				y = myRandom( 20, FRAMEHEIGHT-20);
+			}while(y > FRAMEHEIGHT-20 || x > FRAMEWIDTH-20);
+			
+			System.out.println( "X: "+x+" Y: "+y);
+			
+			Point loc = new Point(x,y);
+			return loc;
+		}
+		
+		public int myRandom(int low, int high) {
+			return (int) (Math.random() * (high - low) + low);
+		}
+		
+
+		class ModuleGUI extends JPanel implements ActionListener {
+			/**
+			 * 
+			 */
+			private final String moduleName = "PopupModule";
+			private final String moduleArea = "Reaction";
+			private final String moduleDescription = "Example Description";
+			private ArrayList<JButton> buttons = null;
+	
+			
+			private JButton runningTest;
+
+			private int buttonCount;
+			private int result;
+
+			public ModuleGUI() {
+				buttons = new ArrayList<JButton>();
+				setLayout(null);
+				setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT);
+				this.add(getIntroductionPanel(timePerButton, numberOfButtons,
+						this));
+			}
+			
+			public JPanel createTestPanel(ActionListener al) {
+				
+				runningTest = generateTest();
+				JPanel testPanel = new JPanel();
+
+				testPanel.setLayout(null);
+				testPanel.setBounds(0, 0, 900, 700);
+				
+				registerButton(runningTest);
+				if (runningTest.getActionListeners().length < 1) {
+					runningTest.addActionListener(al);
+				}
+				testPanel.add(runningTest);
+				return testPanel;
+			}
+
+			public void addTestPanel() {
+				this.removeAll();
+				this.invalidate();
+				this.add(createTestPanel(this));
 				this.revalidate();
-				buttonCount++;
-				break;
-			case 1:// nextModuleButton
-				sendResult(0);
-				tellFinished();
-				break;
+				this.repaint();
 			}
-		}
+			
+			public String getModuleName() {
+				return moduleName;
+			}
 
-		public class NextTask extends TimerTask {
-			@Override
-			public void run() {
-				thisPanel.revalidate();
+			public String getModuleArea() {
+				return moduleArea;
 			}
-		}
 
-		public class NextModule extends TimerTask {
-			@Override
-			public void run() {
-				sendResult(0);
-				tellFinished();
+			public String getModuleDescription() {
+				return moduleDescription;
 			}
+			
+			public void startTest() {
+				setNextModuleTimer(getTime(), new NextModule());
+			}
+			
+			public void registerButton(JButton button) {
+				if (!buttons.contains(button)) {
+					buttons.add(button);
+				}
+			}
+			
+			public class NextModule extends TimerTask {
+				@Override
+				public void run() {
+					result = calculateResult(numberOfButtons, buttonClicked);
+					sendResult(result);
+					tellFinished();
+				}
+			}
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (buttons.contains(e.getSource())) {
+					buttonClicked++;
+					System.out.println("solvedCorrectly " + buttonClicked);
+					if (buttonCounter == numberOfButtons) {
+						result = calculateResult(numberOfButtons, buttonClicked);
+						sendResult(result);
+						tellFinished();
+					}
+					addTestPanel();
+				} else {
+					startTest();
+					addTestPanel();
+				}
+			}
+
 		}
-	}
+	
 }
+
+
 
