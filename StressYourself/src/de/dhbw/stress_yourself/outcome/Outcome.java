85d8a11e388f58e82a1016cdb9acb5c6080dbbea
Tobias Röding <tobias.roeding@googlemail.com>
2012-11-12 17:07:56 +0100

    little adjustment at outcome

--- StressYourself/src/de/dhbw/stress_yourself/outcome/Outcome.java
+++ StressYourself/src/de/dhbw/stress_yourself/outcome/Outcome.java
@@ -1,9 +1,14 @@
 package de.dhbw.stress_yourself.outcome;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import de.dhbw.stress_yourself.params.*;
+import de.dhbw.stress_yourself.params.Parameter;
+import de.dhbw.stress_yourself.params.UserData;
 
 
 public class Outcome {
@@ -19,7 +24,16 @@ public class Outcome {
 
 	public JPanel getOutcomeGUI() {
 		JPanel panel = new JPanel();
-		JLabel label = new JLabel("OUTCOME");
+		JLabel label = new JLabel("You have completed the test! Thank you for your patience!");
+		JButton back = new JButton("Back to Login");
+		back.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
 		panel.add(label);
 		return panel;
 	}
